d48f93da49316448e5d76a4c1a64b1bc86f18a4a
alopezcordero <130616088+alopezccordero@users.noreply.github.com>
2026-05-17 07:27:44 +0000

    created sh

---
+++ huggydog-train.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=huggydog-ppo
+#SBATCH --output=huggy-ppo.out.%j
+#SBATCH --partition=gpua30q
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+#activate conda
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate huggy-rl
+
+
+echo "Python"
+which python
+python --version
+
+echo "Starting huggydog training"
+
+mlagents-learn ./config/ppo/Huggy.yaml \
+    --env=./Huggy \
+    --run-id=huggy-ppo-run-1 \
+    --train \
+    --no-graphics
+
+echo "training finished"
+conda deactivate
